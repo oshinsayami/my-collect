8f565bc7a0a3582e285e3dcaf22cc75ae7eceb99
Simran <mdrsimran@gmail.com>
2020-10-24 10:54:56 -0500

    commit tree f4ea13a6195c337ce77e6e4a7fe64e7fbf5359ad

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(collection)
+    i=0
+    new_array=[]
+    while i<collection.length
+    new_array << yield(collection[i])
+
+    i+=1
+    end
+   new_array
+end
 
 
+# def my_collect(array)
+#     my_collect(array) do |name|
+#         name.upcase
+#     end
+# end
